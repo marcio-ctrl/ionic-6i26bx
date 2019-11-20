d64a7418305d50a3db602020606e1c91d5641c02
marcio-ctrl <marcio.liesenberg@gmail.com>
2019-11-20 16:53:42 -0300

    2 files modified, 1 added, 2 deleted

--- sprite-animation.js
+++ sprite-animation.js
@@ -1,5 +1,3 @@
-  GNU nano 2.9.3                                sprite-animation.js                                             
-
 // Copyright 2013 William Malone (www.williammalone.com)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,12 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
+
 (function() {
-        // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-        // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-        // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-        // MIT license
+	// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+	// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+	// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+	// MIT license
 
     var lastTime = 0;
     var vendors = ['ms', 'moz', 'webkit', 'o'];
@@ -27,3 +25,109 @@
         window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
                                    || window[vendors[x]+'CancelRequestAnimationFrame'];
     }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+(function () {
+			
+	var coin,
+		coinImage,
+		canvas;					
+
+	function gameLoop () {
+	
+	  window.requestAnimationFrame(gameLoop);
+
+	  coin.update();
+	  coin.render();
+	}
+	
+	function sprite (options) {
+	
+		var that = {},
+			frameIndex = 0,
+			tickCount = 0,
+			ticksPerFrame = options.ticksPerFrame || 0,
+			numberOfFrames = options.numberOfFrames || 1;
+		
+		that.context = options.context;
+		that.width = options.width;
+		that.height = options.height;
+		that.image = options.image;
+		
+		that.update = function () {
+
+            tickCount += 1;
+
+            if (tickCount > ticksPerFrame) {
+
+				tickCount = 0;
+				
+                // If the current frame index is in range
+                if (frameIndex < numberOfFrames - 1) {	
+                    // Go to the next frame
+                    frameIndex += 1;
+                } else {
+                    frameIndex = 0;
+                }
+            }
+        };
+		
+		that.render = function () {
+		
+		  // Clear the canvas
+		  that.context.clearRect(0, 0, that.width, that.height);
+		  
+		  // Draw the animation
+		  that.context.drawImage(
+		    that.image,
+		    frameIndex * that.width / numberOfFrames,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height,
+		    0,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height);
+		};
+		
+		return that;
+	}
+	
+	// Get canvas
+	canvas = document.getElementById("coinAnimation");
+	canvas.width = 960;
+	canvas.height = 540;
+	
+	// Create sprite sheet
+	coinImage = new Image();	
+	
+	// Create sprite
+	coin = sprite({
+		context: canvas.getContext("2d"),
+		width: 96000,
+		height: 540,
+		image: coinImage,
+		numberOfFrames: 10,
+		ticksPerFrame: 4
+	});
+	
+	// Load sprite sheet
+	coinImage.addEventListener("load", gameLoop);
+	coinImage.src = "deathbox.png";
+
+} ());
